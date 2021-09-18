f296dfececee4effda418272614daa10837dddc0
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-09-17 18:22:44 -0600

    i think this is it

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-console.log("why you no working")
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+]
+
+
+
 function updateContent() {
     console.log("update content ran")
-    const links = [
-        {
-          label: "Week1 notes",
-          url: "week1/index.html"
-        }
-      ]
 
     var content = document.getElementById('content');
     console.log("this is working")
     
     links.forEach(element => {
-        content.innerHTML += "<li>" + links.label + "</li>";
+        content.innerHTML += "<li>" + links.label.this + "</li>";
     });
 }
