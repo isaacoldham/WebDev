5039d67a416415fb345275f6aea03c6f13163cf0
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-09-17 18:07:54 -0600

    continue with testing

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function updateContent() {
 
     var content = document.getElementById('content');
     content.innerHTML += "<li>" + label + "<li>";
+    console.log("this is working")
     
     links.forEach(label => {
         content.innerHTML += "<li>" + label + "<li>";
