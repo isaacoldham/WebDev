38da562095f35a4cf6ca9db4d5ef5b0ac16ac378
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-10-19 13:59:20 -0600

    try this one

--- week6/script.js
+++ week6/script.js
@@ -1,6 +1,51 @@
 let toDoHTML = '<li class="listItem">Task</li>'
-let toDo = {
+let toDoList = [
+{
+    id: Date.now(),
+    content: 'Add a task to start your to do list!',
+    completed: false
+},
+{
+    id: Date.now(),
+    content: 'Testing',
+    completed: true
+},
+{
+    id: Date.now(),
+    content: 'another test',
+    completed: false
+},
+{
     id: Date.now(),
     content: 'Add a task to start your to do list!',
     completed: false
 }
+]
+
+
+
+
+window.addEventListener("load", () => { showToDoList(); });
+
+function showToDoList() {
+    const toDoListElement = document.getElementById("toDoList");
+    toDoListElement.innerHTML = "";
+    renderToDoList(toDoList, toDoListElement);
+  }
+  
+  function renderToDoList(toDos, parent) {
+    toDos.forEach(toDo => {
+      parent.appendChild(renderOneTask(toDo));
+    });
+  }
+  function renderOneTask(toDo) {
+    const item = document.createElement("li");
+  
+    item.innerHTML = toDo.content;
+    item.cl
+  
+    return item;
+  }
+  
+
+console.log(toDo)
