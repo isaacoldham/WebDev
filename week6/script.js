ca1dfc795ee419ec3a49ef6becd6056aa9e65758
isaacoldham <59744617+isaacoldham@users.noreply.github.com>
2021-10-20 14:35:04 -0600

    ToDo assingment
    That should pretty much wrap up all the necessary requirements for the To Do assignment

--- week6/script.js
+++ week6/script.js
@@ -12,8 +12,15 @@ let toDoList = [
 
 
 window.addEventListener("load", () => { showToDoList(); });
-document.getElementById("submit").addEventListener("click",() => { addTask(); });
-document.getElementById("").addEventListener("click",() => { addTask(); });
+document.getElementById("submit").addEventListener("click", () => { addTask(); });
+document.getElementById("completeTasks").addEventListener("click", () => { filterComplete(); })
+document.getElementById("incompleteTasks").addEventListener("click", () => { filterIncomplete(); })
+document.getElementById("allTasks").addEventListener("click", () => { showToDoList(); })
+
+
+
+
+
 
 function showToDoList() {
     const toDoListElement = document.getElementById("toDoList");
@@ -27,34 +34,101 @@ function renderToDoList(toDos, parent) {
 }
 function renderOneTask(toDo) {
     const item = document.createElement("li");
-
-    item.innerHTML = toDo.content;
+    item.innerHTML = `<input class="check" type="checkbox" id="check${toDo.id}" onClick="check(this.id)">
+                      ${toDo.content}
+                      <input class="delete" type="button" id="del${toDo.id}" onClick="delTask(this.id)" value ="Remove">`;
+    item.id = toDo.id;
+    console.log(item);
     if (toDo.completed == true) {
-        item.classList.add("checked")
+        console.log("this is the check if")
+        item.classList.add("checked");
+        return item;
+    } else {
+        return item;
     }
-    else if (toDo.completed == false) {
-        item.classList.remove("checked");
-    }
-
-    return item;
 }
 
 function addTask() {
-    console.log("addTask ran")
     let newTask = document.getElementById("newTask").value;
     if (newTask == "")
         return;
     else {
-        toDoList.push({id: Date.now(), content: newTask, completed: false})
+        toDoList.push({ id: Date.now(), content: newTask, completed: false })
         document.getElementById("newTask").value = "";
+
         showToDoList();
     }
 }
 
+function check(taskid) {
+    let id = taskid.replace('check', '');
+    item = document.getElementById(id);
+    for (let key in toDoList) {
+        if (toDoList[key].id == id) {
+            if (toDoList[key].completed == false) {
+                item.classList.add("checked");
+                toDoList[key].completed = true;
+            }
+            else if (toDoList[key].completed == true) {
+                item.classList.remove("checked");
+                toDoList[key].completed = false;
+                console.log('should be removed')
+            }
+        }
+    }
+}
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+function delTask(taskid) {
+    let id = taskid.replace('del', '');
+    item = document.getElementById(id);
+    for (let key in toDoList) {
+        if (toDoList[key].id == id) {
+            toDoList.splice(key, 1);
+            showToDoList();
+        }
+    }
+}
+
+function filterComplete() {
+    const completed = true;
+    const toDoListElement = document.getElementById("toDoList");
+    toDoListElement.innerHTML = "";
+    filterToDoList(toDoList, toDoListElement, completed);
+}
+function filterToDoList(toDos, parent, completed) {
+    toDos.forEach(toDo => {
+        parent.appendChild(renderOneFilteredTask(toDo, completed));
+    });
+}
+function renderOneFilteredTask(toDo, completed) {
+    if (toDo.completed == completed) {
+        const item = document.createElement("li");
+        item.innerHTML = `<input class="check" type="checkbox" id="check${toDo.id}" onClick="check(this.id)">
+                      ${toDo.content}
+                      <input class="delete" type="button" id="del${toDo.id}" onClick="delTask(this.id)" value ="Remove">`;
+        item.id = toDo.id;
+        console.log(item);
+        console.log(toDo.completed)
+        console.log(completed)
+        if (toDo.completed == true) {
+            console.log("this is the check if")
+            item.classList.add("checked");
+            return item;
+        } else {
+            return item;
+        }
+    }
+    else {
+        console.log("this is the else")
+        const item = document.createElement("li");
+        item.style.display = "none";
+        return item;
+    }
+}
+
+function filterIncomplete() {
+    const completed = false;
+    const toDoListElement = document.getElementById("toDoList");
+    toDoListElement.innerHTML = "";
+    filterToDoList(toDoList, toDoListElement, completed);
+}
